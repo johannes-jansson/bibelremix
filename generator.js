f4a829baf9811484e8603afa17582618985ccc9c
johannes-jansson <johannes.e.jansson@gmail.com>
2018-10-21 17:18:33 +0200

    implemented walkChain

--- generator.js
+++ generator.js
@@ -1,13 +1,10 @@
 const test = 'And the Golden Grouse And the Pobble who';
 
-// const wordChain = (wordTransitions) => {
-//   console.log(wordTransitions);
-// };
-
 const textToWordChain = (s) => {
   const l = s.toLowerCase().split(' ');
+  l.push(undefined);
   const map = {};
-  for (let i = 0; i < l.length - 2; i++) {
+  for (let i = 0; i < l.length - 1; i++) {
     const key = [l[i], l[i + 1]];
     if (!map[key]) {
       map[key] = [];
@@ -17,16 +14,41 @@ const textToWordChain = (s) => {
   return map;
 };
 
-console.log(textToWordChain(test));
+const testChain = textToWordChain(test);
+console.log(testChain);
 
 // const chainToText = (chain) => {
 //   console.log(chain);
 // };
 
-// const walkChain = (prefix, chain) => {
-//   console.log(prefix);
-//   console.log(chain);
-// };
+const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+const walkChain = (prefix, chain) => {
+  const result = prefix.split(',');
+  console.log(result);
+  let possibilities = chain[prefix];
+  console.log(possibilities);
+  if (!possibilities || possibilities[0] === undefined) {
+    return result;
+  }
+  possibilities = shuffle(possibilities);
+  // console.log(possibilities);
+  result.push(possibilities[0]);
+  console.log(result);
+  const newPrefix = result[1] + ',' + result[2];
+  console.log(newPrefix);
+  return [result[0]].concat(walkChain(newPrefix, chain));
+};
+
+console.log(walkChain('the,pobble', testChain));
+console.log(walkChain('and,the', testChain));
+// console.log(walkChain('pobble,who', testChain));
 
 // const generateText = (startPhrase, wordChain) => {
 //   console.log(startPhrase);
