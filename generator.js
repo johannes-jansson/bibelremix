9f02a065e322d95fa392128c793a08b59aa45234
johannes-jansson <johannes.e.jansson@gmail.com>
2018-10-22 22:30:40 +0200

    show more button working

--- generator.js
+++ generator.js
@@ -45,9 +45,28 @@ const generateText = (startPhrase, wordChain) =>
 const chain = textToWordChain(text);
 
 document.addEventListener('click', function (event) {
-  if (!event.target.matches('#generate-button')) return;
-  event.preventDefault();
-  const userPrefix = document.getElementById('input-prefix').value;
-  const verse = generateText(userPrefix, chain);
-  document.getElementById('textplace').innerHTML = '<p>' + verse + '</p>';
+  if (event.target.matches('#generate-button')) {
+    event.preventDefault();
+    const userPrefix = document.getElementById('input-prefix').value;
+    const verse = generateText(userPrefix, chain);
+    document.getElementById('textplace').innerHTML = '<p>' + verse + '</p>';
+    document.getElementById('more-button').style.display = 'inline';
+  }
+  if (event.target.matches('#more-button')) {
+    console.log('test');
+    event.preventDefault();
+    const currentText = document.getElementById('textplace').innerHTML.replace(/<p>/g, '').replace(/<\/p>/g, '');
+    let userPrefix = currentText.split(' ');
+    userPrefix = userPrefix[userPrefix.length - 2] + ' ' + userPrefix[userPrefix.length - 1];
+    let verse = generateText(
+      userPrefix.substring(0, userPrefix.length - 1),
+      chain,
+    );
+    verse = verse.split(' ');
+    verse.shift();
+    verse.shift();
+    verse = verse.join(' ');
+    verse = verse.charAt(0).toUpperCase() + verse.slice(1);
+    document.getElementById('textplace').innerHTML = '<p>' + currentText + ' ' + verse + '</p>';
+  }
 });
